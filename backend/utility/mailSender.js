db25d6fb4389beabcc5a91617927570f6d69ba8e
Nithin0620 <nithin30302@gmail.com>
2026-06-03 21:11:16 +0530

    fix(api); change smtp port to 465.

--- backend/utility/mailSender.js
+++ backend/utility/mailSender.js
@@ -5,6 +5,8 @@ exports.sendEmail = async(email,title,body)=>{
    try{
       const transporter = nodemailer.createTransport({
          host:process.env.MAIL_HOST,
+         port: 465,
+         secure: true,
          auth :{
             user: process.env.MAIL_USER,
             pass : process.env.MAIL_PASS
